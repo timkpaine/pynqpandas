6964c781c73870c4779fb7e3f4cf4142b1b9142c
Tim Paine <t.paine154@gmail.com>
2017-10-26 17:13:59 -0400

    setting up repo

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pynqpandas',
+    version='0.0.1',
+    description='Hardware-accelerated Pandas',
+    long_description=long_description,
+    url='https://github.com/timkpaine/pynqpandas',
+    download_url='https://github.com/timkpaine/pynqpandas/archive/v0.0.4.tar.gz',
+    author='Tim Paine',
+    author_email='timothy.k.paine@gmail.com',
+    license='LGPL',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='pandas fpga',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=[]),
+)
