0eb494593bb19dcd79070ff9fe6f9f188c343a56
Tim Paine <t.paine154@gmail.com>
2017-11-30 19:53:15 -0500

    .

--- hw/tb/tb.sv
+++ hw/tb/tb.sv
@@ -8,15 +8,41 @@
 
 /* the testbench */
 `ifdef CC_VCS
+`define MOD ds.cb.
 program tb(ifc.bench ds);
 `else
+`ifdef CC_MODELSIM
+`define MOD ds.cb.
 module tb(ifc.bench ds);
+`else 
+`define MOD 
+`include "dut.sv"
+module tb;
+`endif
 `endif
 
     transaction t;
     testing_env v;
     operations o;
 
+`ifdef CC_VIVADO
+    logic clk;
+    logic reset;
+    logic signed [NUM_SIZE-1: 0] in1;
+    logic signed [NUM_SIZE-1: 0] in2;
+    logic [2**CMD_SIZE_LOG2-1:0] cmd;
+    logic out;
+
+    dut dut(.clk(clk),
+      .reset(reset),
+      .in1(in1),
+      .in2(in2),
+      .cmd(cmd),
+      .out(out)
+    );
+
+    always #5 clk = ~clk;
+`else
     /* reset vars */
     bit reset;
 
@@ -24,18 +50,18 @@ module tb(ifc.bench ds);
     int op;
     int in1;
     int in2;
-
-    
-
+`endif
+   
     initial begin
         t = new();
         v = new();
         o = new();
-
         v.read_config("./config.txt");
-
         repeat(10) begin
-            o.flush(ds.cb.reset, ds.cb.in1, ds.cb.in2, ds.cb.cmd);
+            ds.cb.reset = 1'b1;
+            ds.cb.in1 = 'b0;
+            ds.cb.in2 = 'b0;
+            ds.cb.cmd = 'b0;
             @(ds.cb);
         end
        @(ds.cb);
@@ -62,23 +88,72 @@ module tb(ifc.bench ds);
 
     /* function for running reset tests */
     task run_reset();
-        o.run_reset(v, t, ds.cb.reset, ds.cb.out);
+        reset = v.get_reset();
+        
+        // drive inputs for next cycle
+        if(reset) begin
+            ds.cb.reset = 1'b1;
+            $display("%t : %s", $realtime, "Driving Reset");
+        end else begin
+            ds.cb.reset = 1'b0;
+        end
         @(ds.cb);
         ds.cb.reset <= 1'b0;
         @(ds.cb);
-        o.check_reset(v, t, ds.cb.reset, ds.cb.out);
+        //golden results
+        if( reset ) begin
+            $display( "%t : %s", $realtime, t.check_reset( ds.cb.out ) ? "Pass-reset":"Fail-reset" );
+            t.drive_reset();
+        end else begin
+            $display( "%t : %s", $realtime, t.check_not_reset( ds.cb.out ) ? "Pass-not-reset" : "Fail-not-reset" );
+        end
+        // t.clock_tic();
     endtask : run_reset
 
     task run_op();
-        o.run_op(v, t, ds.cb.cmd, ds.cb.in1, ds.cb.in2, ds.cb.out);
+        op = v.get_op();
+
+        // drive inputs for next cycle
+        if( op > 0 ) begin
+            ds.cb.cmd = op;
+            ds.cb.in1 = v.in1;
+            ds.cb.in2 = v.in2;
+            case( op )
+                ADD: begin
+                  $display("%t : %s + %d %d ", $realtime, "Driving op  ", v.in1, v.in2);
+                end
+                NOOP: begin
+                  $display("%t : %s", $realtime, "Driving noop ");
+                end
+            endcase
+          t.drive_op( op, v.in1, v.in2 );
+
+        end else begin
+            ds.cb.cmd = NOOP;
+            ds.cb.in1 = 'b0;
+            ds.cb.in2 = 'b0;
+        end
         @(ds.cb);
         ds.cb.cmd = NOOP;
         @(ds.cb);
+        //golden results
+        if( op ) begin
+            case( op )
+                ADD: begin
+                    $display( "%t : %s %d %d", $realtime, t.check_op( ds.cb.out ) ? "Pass-op +" : "Fail-op +", t.out, ds.cb.out);
+                end
+            endcase
+        end else begin
+          $display( "%t : %s %d %d", $realtime, t.check_noop( ds.cb.out ) ? "Pass-noop" : "Fail-noop", t.out, ds.cb.out);
+        end
+        // t.clock_tic();
     endtask : run_op
 
+
+/* the testbench */
 `ifdef CC_VCS
-endprogram //tb
+endprogram
 `else
-endmodule //tb
+endmodule 
 `endif
 `endif
